c06a1f3278cde18771cf4aeffd22f1970b9d4a27
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2020-10-15 09:33:06 +0530

    Tem

--- api/chromium.js
+++ api/chromium.js
@@ -20,9 +20,18 @@ async function getPage() {
 }
 
 export async function getScreenshot(html) {
-    const page = await getPage();
-    await page.setViewport({ width: 2048, height: 1170 });
-    await page.setContent(html);
-    const file = await page.screenshot({ type });
-    return file;
+    // const page = await getPage();
+    // await page.setViewport({ width: 2048, height: 1170 });
+    // await page.setContent(html);
+    // const file = await page.screenshot({ type });
+    // return file;
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://example.com');
+        const file =  await page.screenshot({path: 'example.png'});
+      
+        await browser.close();
+        return file;
+    })();
 }
