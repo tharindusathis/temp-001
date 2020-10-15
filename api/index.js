1b400af99099a776084eeba92769cf6ced7852e7
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2020-10-15 09:53:47 +0530

    Tem

--- api/index.js
+++ api/index.js
@@ -15,20 +15,11 @@ async function getPage() {
 }
 
 async function getScreenshot(html) {
-    // const page = await getPage();
-    // await page.setViewport({ width: 2048, height: 1170 });
-    // await page.setContent(html);
-    // const file = await page.screenshot({ type });
-    // return file;
-    (async () => {
-        const browser = await launch();
-        const page = await browser.newPage();
-        await page.goto('https://example.com');
-        const file =  await page.screenshot({path: 'example.png'});
-      
-        await browser.close();
-        return file;
-    })();
+    const page = await getPage();
+    await page.setViewport({ width: 2048, height: 1170 });
+    await page.setContent(html);
+    const file = await page.screenshot({ type });
+    return file;
 }
 
 export default (req, res) => {
